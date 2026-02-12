b44be0fce96fcdab347a568552fe6bc8a9d96900
drac-0 <muhammadfaiz050607@gmail.com>
2026-02-12 22:05:16 +0700

    important function

--- scratching data structure/stack.c
+++ scratching data structure/stack.c
@@ -1,6 +1,7 @@
 #include <bits/types/stack_t.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define STACKLEN 10
 
@@ -10,31 +11,57 @@ typedef struct{
 
 }Stack ;
 
+void initstack(Stack *stack){
+  stack->topi = -1;
+}
+
+bool emptycheck(Stack *stack){
+  return stack->topi == -1;;
+}
+
+bool full(Stack *stack){
+  return stack->topi >= STACKLEN - 1;
+}
+
 void push(int val, Stack *stack){
   if (stack->topi > STACKLEN - 1){
     printf("STACK OVERLOAD\n");
   }
-  stack->arr[stack->topi] = val;
   stack->topi++;
+  stack->arr[stack->topi] = val;
 }
 
 void printstack(Stack *stack){
-  for (int i = 0 ; i < stack->topi; i++){
-    printf("[ %d ]\n", stack->arr[stack->topi - i - 1]);
+  for (int i = 0 ; i <= stack->topi; i++){
+    printf("[ %d ]\n", stack->arr[stack->topi - i ]);
   }
 }
 
-void pop(Stack *stack){
+int pop(Stack *stack){
+  if (emptycheck(stack)){
+    printf("empty stack\n");
+    return -1 ;
+  }
 
+  int popped = stack->arr[stack->topi];
+  stack->topi--;
+  return popped;
 }
 
 int main(){
   Stack s1 ;
+  initstack(&s1);
   push(1, &s1);
   push(2, &s1);
   push(3, &s1);
   push(7, &s1);
-
   printstack(&s1);
+
+  printf("%d\n", pop(&s1));
+  printstack(&s1);
+  printf("\n");
+  push(9, &s1);
+  printstack(&s1);
+  
 }
 
