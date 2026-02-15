69d5775cd9414fc10edc9ad8fac7c265ab115e58
drac-0 <muhammadfaiz050607@gmail.com>
2026-02-15 18:35:59 +0700

    suppose to be doubly linked

---
+++ scratching data structure/doll.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LL {
+  int val;
+  struct LL *next;
+  struct LL *prev;
+}DLL;
+
+void listprint(DLL *head){
+  DLL *temp = head;
+  while (temp != NULL){
+    printf("%d - ", temp->val);
+    temp = temp->next;
+  }
+  printf("END\n");
+}
+
+DLL *append(int val ){
+  DLL *newnode = malloc(sizeof(DLL));
+  newnode->val = val;
+  newnode->next = NULL;
+  return newnode;
+}
+
+DLL *movetohead(DLL **head, DLL *node){
+  node->next = *head;
+  *head = node;
+  return node;
+}
+
+DLL *findval (DLL *head, int valtofind){
+  DLL *temp = head;
+  while(temp != NULL){
+    if (temp->val == valtofind)return temp;
+    temp = temp->next;
+  }
+  return NULL;
+}
+
+int main(){
+  DLL *head ;
+  DLL *temp ;
+
+  //linked by default
+  temp = append(5);
+  head = temp;
+  temp = append(3);
+  temp->next = head;
+  head = temp;
+  temp = append(1);
+  temp->next = head;
+  head = temp;
+
+  listprint(head);
+
+  //linked by loop and function
+  for (int i = 0 ; i < 10 ; i++){
+    temp = append(i);
+    movetohead(&head, temp);
+    //temp->next = head; //temp.next become NULL
+    //head = temp; //temp becomes head
+  }
+
+  listprint(head);
+
+  temp = findval(head, 0);
+  printf("%d found inside the ll\n", temp->val);
+  
+
+}
