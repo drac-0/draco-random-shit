39d458168e52286777aef91967971e9f377b696f
drac-0 <muhammadfaiz050607@gmail.com>
2026-02-20 03:45:34 +0700

    method update

--- materiajarll/linkedlist.java
+++ materiajarll/linkedlist.java
@@ -13,9 +13,9 @@ class linkedlist{
   //method untuk menambahkan anggota
   //lihat contoh penggunaannya pada file App.java
   void add(int newval){
-    Node newnode = new Node();
-    newnode.val = newval;
-    newnode.next = head;
+    Node newnode = new Node(); // kelas baru dideklarasikan dengan seluruh atribut bernilai 0
+    newnode.val = newval; //atribut val dideklarasikan
+    newnode.next = head; //atribut next dideklarasikan
     head = newnode;
   }
 
@@ -24,11 +24,45 @@ class linkedlist{
     Node temp = head; 
     String result = "[";
     while (temp != null){
-      result = result + " "+ Integer.toString(temp.val);
+      result = result + " " + Integer.toString(temp.val) + " - ";
       temp = temp.next;
     }
-
+    result += " END";
     result = result + " ]"; 
     return result;
+  }
+
+  public String checkval(int fi){
+    Node C = head ;
+    System.out.println("loop start");
+    while(C != null){
+      System.out.println("C val" +C.val);
+      System.out.println("fi " + fi);
+      if (C.val == fi){
+        System.out.println("test");
+        return "ADA";}
+      C = C.next;
+    }
+    return null;
+  } 
+
+  public Node find(int fi){
+    Node C = head ;
+    while(C != null){
+      if (C.val == fi){
+        return C;}
+      C = C.next;
+    }
+    return null;
+  }
+
+  public Node insert(int fi, int tval){
+    Node C = head ;
+    while(C != null){
+      if (C.val == fi){
+        return C;}
+      C = C.next;
+    }
+    return null;
   }
 }
