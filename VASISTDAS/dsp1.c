d4a41f3f0a2385a361baa1815b715da7bd2bcd44
drac-0 <muhammadfaiz050607@gmail.com>
2026-03-18 02:04:06 +0700

    c scratch

--- VASISTDAS/dsp1.c
+++ VASISTDAS/dsp1.c
@@ -6,17 +6,36 @@ typedef struct{
       void (*func)(int a,int b);
 }hashmap;
 
+hashmap * hastable[10];
+
+
 void hash(hashmap *hm, char *key, void (*dummy)(int a, int b)){
       hm->key = key;
       hm->func = dummy;
 }
 
-void test(){
-      printf("dummy function");
+void test(int a, int b){
+      printf("%d\n", a + b);
+}
+
+void printhash(){
+      for (int i = 0 ; i < 10; i++){
+            if (hastable[i] == NULL ){
+                  printf("DONE");
+                  break ;
+            }
+            printf("%s\n",hastable[i]->key);
+      }
 }
 
 int main(){
       hashmap d1 ;
-      hash(&d1, "test",test);
+      hash(&d1, "key1",test);
+      hastable[0] = &d1;
+      printf("%s\n", hastable[0]->key);
+      hastable[0]->func(1,2);
+      printhash();
+
       return 0;
+
 }
